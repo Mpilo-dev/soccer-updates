ecac953dc6cc83d4e43ffa5a64b498ce5871d636
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2024-01-09 14:28:06 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Soccer-Updates`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
